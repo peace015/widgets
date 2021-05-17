768d076e2b60a3a554f4e10a97a758f8a3c80679
Panupong P <panupong.pruk@gmail.com>
2021-05-17 23:18:33 +0700

    add components add helper fuction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'what is react?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'why use react?',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
---
+++ src/components/Accordion.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Accordion = ({ items }) => {
+  const onTitleClick = (index) => {
+    console.log('Title clicked', index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active" onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
+
+export default Accordion;
