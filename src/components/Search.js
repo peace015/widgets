0f77144ccb55423c31bf4eb8f3759ff7e6e10071
Panupong P <panupong.pruk@gmail.com>
2021-05-20 10:50:01 +0700

    add dangerouselyHTML add wiki api continue use effect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
 
-  useEffect(() => {});
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    search();
+  }, [term]);
+
+  const renderResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -17,6 +46,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderResults}</div>
     </div>
   );
 };
