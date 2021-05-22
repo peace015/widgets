498883618d0206839c47d2b7fcfe160c04837f9f
Panupong P <panupong.pruk@gmail.com>
2021-05-22 09:56:18 +0700

    add dropdown test another widgets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
@@ -16,11 +17,25 @@ const items = [
     content: 'You use React by creating components',
   },
 ];
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue',
+  },
+];
 
 export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
---
+++ src/components/Dropdown.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Dropdown = ({ options }) => {
+  const renderOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select color</div>
+          <div className="menu visible transition">{renderOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
