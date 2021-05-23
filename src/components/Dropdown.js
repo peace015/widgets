e8cb5399b9be2c1893c22146050855212cb836a0
Panupong P <panupong.pruk@gmail.com>
2021-05-23 23:41:06 +0700

    add more usestate to fix dropdown selection by make it tokkle

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
   const renderOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
     return (
       <div
         key={option.value}
@@ -17,10 +23,15 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderOptions}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
+            {renderOptions}
+          </div>
         </div>
       </div>
     </div>
